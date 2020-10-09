07a1e896f1bd57a91ad8bdc64d3bfae818c9f966
Cameron Duncan <cameronduncan678@gmail.com>
2020-10-09 13:09:00 +0100

    Prototype design and frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/css/App.css';
+
+//Components
+import Header from './components/main/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
