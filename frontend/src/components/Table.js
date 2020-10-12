39af89580e85025ade5654cd06cb85e4b995d83a
Cameron Duncan <cameronduncan678@gmail.com>
2020-10-12 22:03:09 +0100

    table rows added

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -3,8 +3,112 @@ import React from 'react';
 class Table extends React.Component{
     render(){
         return(
-            <div className="wh-table">
-
+            <div className="wh-table-section">
+                <table className="wh-table">
+                    <thead className="wh-table-thead">
+                        <tr className="wh-table-headers">
+                            <th className="wh-table-header-1">Order ID</th>
+                            <th className="wh-table-header-2">Name</th>
+                            <th className="wh-table-header-3">Item Amount</th>
+                            <th className="wh-table-header-4">Deadline</th>
+                            <th className="wh-table-header-5">Status</th>
+                            <th className="wh-table-header-6">Location</th>
+                            <th className="wh-table-header-7"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>28bvao85ff</td>
+                            <td>Jane Lancaster</td>
+                            <td>#29</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-warning">WARNING</div></td>
+                            <td>Derby</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>09vboav23biv</td>
+                            <td>Allister McBrindle</td>
+                            <td>#07</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-over">OVER</div></td>
+                            <td>Derby</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                        <tr className="wh-table-data">
+                            <td>223hobo5b2pco</td>
+                            <td>Jackob Soldin</td>
+                            <td>#13</td>
+                            <td>12/10/2020</td>
+                            <td><div className="wh-table-status-ok">STORAGE</div></td>
+                            <td>Manchester</td>
+                            <td><div className="wh-table-link-btn"><i className="fas fa-external-link-square-alt"></i></div></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
