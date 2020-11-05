a60a24ed1880748207ce60d1d4027067e2872eb6
Cameron Duncan <cameronduncan678@gmail.com>
2020-11-05 22:14:21 +0000

    table and generator testing files

---
+++ design/idGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using IdGen;
+using Unified;
+
+namespace idGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomIdGen()
+            {
+                string id = UnifiedId.NewId();
+                return id;
+            }
+
+            int maxLines = 50;
+
+            string[] randomIds = new string[50];
+
+            for (int i = 0; i < maxLines; i++)
+            {
+                string newId = randomIdGen();
+
+                randomIds[i] = newId;
+
+            }
+
+            System.IO.File.WriteAllLines(@"C:\Users\Cameron\Documents\CodingProgramming\Projects\warehouseDBMS\backend\database\design\idGenerator\randomIds.txt", randomIds);
+
+        }
+    }
+}
