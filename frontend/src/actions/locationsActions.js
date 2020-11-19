55a31126705d6060acf2aa6c63a32f7e77b65fb6
Cameron Duncan <cameronduncan678@gmail.com>
2020-11-19 18:25:03 +0000

    location endpoint hooked to app

--- frontend/src/actions/locationsActions.js
+++ frontend/src/actions/locationsActions.js
@@ -1,14 +1,18 @@
 import { FETCH_LOCATIONS } from './types';
 import axios from 'axios';
 
+var dummy = 'https://s3-eu-west-1.amazonaws.com/warehouse.data.placeholder/renting.json';
+var localhost = 'http://localhost:5555/location';
+
 export const fetchLocations = () => dispatch => {
-    axios.get('https://s3-eu-west-1.amazonaws.com/warehouse.data.placeholder/locations.json',
+    axios.get(localhost,
         { headers: { "Access-Control-Allow-Origin": "*" } })
         .then((packet) => {
             dispatch({
                 type: FETCH_LOCATIONS,
-                payload: packet.data.locations
+                payload: packet.data
             })
+            console.log(packet.data);
         })
 }
 
