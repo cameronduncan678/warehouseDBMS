16f81cea09e7074c55f8ea2f0d7f6b485d1064ac
Cameron Duncan <cameronduncan678@gmail.com>
2020-12-24 14:56:41 +0000

    targets PUT method added

---
+++ backend/warehousedbms/Controllers/TargetsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using warehousedbms.Data;
+using warehousedbms.Models;
+
+namespace warehousedbms.Controllers
+{
+    [EnableCors("MyPolicy")]
+    [Route("targets")]
+    public class TargetsController: ControllerBase
+    {
+        
+        private readonly DataContext _dbContext;
+        
+        public TargetsController(DataContext dataContext)
+        {
+            _dbContext = dataContext;
+        }
+
+        [HttpPut("{locationId}")]
+        public async Task<IActionResult> NewTargets(string locationId, [FromBody] RawTargets _targets)
+        {
+            Targets _data = _dbContext.Targets.Single(d => d.locationId == locationId);
+
+            if (_data != null)
+            {
+                foreach (var dat in _dbContext.Targets)
+                {
+                    if (dat.locationId == locationId)
+                    {
+                        dat.income = _targets.income;
+                        dat.perc = _targets.perc;
+                        dat.slots = _targets.slots;
+                        dat.spaces = _targets.spaces;
+                    }
+                }
+
+                await _dbContext.SaveChangesAsync();
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ backend/warehousedbms/Models/RawTargets.cs
@@ -0,0 +1,10 @@
+﻿namespace warehousedbms.Models
+{
+    public class RawTargets
+    {
+        public int perc { get; set; }
+        public int slots { get; set; }
+        public int spaces { get; set; }
+        public int income { get; set; }
+    }
+}
